88726489c423a08279aec71ebbd02a7890933f44
Alexandr <zima220410@gmail.com>
2022-05-11 18:24:35 +0300

    11.05

--- objects.js
+++ objects.js
@@ -1,6 +1,27 @@
-'use strict';
-// Объекты
-// 1.	Клиенты банка, имеют такие характеристики - фио, активный или нет, дата регистрации в банке, счета. Существует два типа счетов: дебетовый и кредитовый. Дебитовый счет имеет текущий баланс либо он положителен либо нулевой. Кредитовый счет имеет два баланса: личные средства, кредитные средства и кредитный лимит. У каждого счета есть активность, дата активности когда заканчивается срок годности пластиковой карты. У каждого счета есть тип валюты, UAH, RUB, USD, GBP, EUR и другие. Подсчитать общее количество денег внутри банка в долларовом эквиваленте учитывая кредитные лимиты и снятие средств. Посчитать сколько всего денег в долларовом эквиваленте все клиенты должны банку. Посчитать сколько неактивных клиентов должны погасить кредит банку и на какую общую сумму. Аналогично для активных. Для получения актуальных курсов валют использовать API (которое будет предоставлено). Промисы использовать для работы с API в целях отправки запросов на сервер. Создать отдельный git-репозиторий для этого проекта и дальше работать с этим проектом в этом репозитории.
+let addForm = document.querySelector('.add_client');
+let addId = document.querySelector('.add_id_client');
+let addName = document.querySelector('.add_name_client');
+let addIsActive = document.querySelector('.add_isactive');
+let findForm = document.querySelector('.find');
+let idByFind = document.querySelector('.find_by_id');
+let addFormDebAccount = document.querySelector('.add_debit_account');
+let addDebitCurrency = document.querySelector('.add_debit_currency');
+let addDebitBalance = document.querySelector('.add_debit_balance');
+let addFormCreditAccount = document.querySelector('.add_credit_account');
+let addCreditCurrency = document.querySelector('.add_credit_currency');
+let addCreditBalance = document.querySelector('.add_credit_balance');
+let addCreditLimit = document.querySelector('.add_credit_limit');
+let resultAdding = document.querySelector('.res_adding');
+let resultDebAccount = document.querySelector('.res_deb_account');
+let resultCreditAccount = document.querySelector('.res_credit_account');
+let resultFinding = document.querySelector('.res_finding');
+let resultFindingDebAccount = document.querySelector('.res_finding_deb_account');
+let resultFindingCreditAccount = document.querySelector('.res_finding_cred_account');
+let buttonSumBankMoney = document.querySelector('#sum');
+let buttonSumDebitBank = document.querySelector('#deb_sum');
+let buttonNumbersDebitors = document.querySelector('#num_deb');
+let buttonSumDebitIsactiveClients = document.querySelector('#sum_deb_act_clients');
+let totalResult = document.querySelector('.total_result');
 
 class BankClient {
     constructor(id, name, isActive) {
@@ -11,113 +32,201 @@ class BankClient {
         this.debitAccounts = [];
         this.creditAccounts = [];
     }
-    addDebitAccount(currency, expirationDate, isActive, dateLastActivity, balance) {
-        this.debitAccounts.push(new Account(currency, expirationDate, isActive, dateLastActivity, balance));
-    }
-    addCreditAccount(currency, expirationDate, isActive, dateLastActivity, balance, creditLimit) {
-        this.creditAccounts.push(new Account(currency, expirationDate, isActive, dateLastActivity, balance, creditLimit));
-    }
 }
 
 class Account {
-    constructor(currency, expirationDate, isActive, dateLastActivity, balance, creditLimit = null) {
+    constructor(currency, balance, creditLimit = null) {
         this.currency = currency;
-        this.expirationDate = expirationDate;
-        this.isActive = isActive;
-        this.dateLastActivity = dateLastActivity;
         this.balance = balance;
         this.creditLimit = creditLimit;
     }
 }
 
-export class Bank {
-    constructor() {
-        this.bank = [];
-    }
+let bank = [];
 
-    addClient(id, name, isActive) {
-        this.bank.push(new BankClient(id, name, isActive));
-    }
+function addClient(id, name, isActive) {
+    bank.push(new BankClient(id, name, isActive));
+}
 
-    async bankMoney(arrayBankCustomers) {
-        let sum = 0;
-        await this.requestExchangeRate().then(exchangeRates => {
-            arrayBankCustomers.forEach(client => {
-                [...client.creditAccounts, ...client.debitAccounts].forEach(account => sum += currencyConversion(account, account.balance, exchangeRates));
-            });
-        });
-        return sum;
+addForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let newClient = true;
+    bank.forEach(client => {
+        if (client.id === addId.value) {
+            newClient = false;
+        }
+    });
+    if (newClient) {
+        if (/^[0-9]+$/.test(addId.value) && /^[a-z\s]+$/i.test(addName.value)) {
+            addClient(addId.value, addName.value, addIsActive.checked);
+            resultAdding.innerHTML = `ID - ${addId.value} , Name - ${addName.value} , isActive - ${addIsActive.checked}`;
+            clearAddForm();
+        } else {
+            resultAdding.innerHTML = 'Данные для ввода не корректны';
+            clearAddForm();
+        }
+    } else {
+        resultAdding.innerHTML = 'Этот клиент уже внесен!';
+        clearAddForm();
     }
+    event.target.reset();
+});
 
-    async bankDebit(arrayBankCustomers) {
-        let sum = 0;
-        let debtBalance = 0;
-        await this.requestExchangeRate().then(exchangeRates => {
-            arrayBankCustomers.forEach(client => {
-                client.creditAccounts.forEach(account => {
-                    if (account.creditLimit > account.balance) {
-                        debtBalance = account.creditLimit - account.balance;
-                        sum += this.currencyConversion(account, debtBalance, exchangeRates);
-                    }
-                });
-            });
-        });
-        return sum;
+function clearAddForm() {
+    resultDebAccount.innerHTML = '';
+    resultCreditAccount.innerHTML = '';
+}
+
+addFormDebAccount.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let debitBalance = +addDebitBalance.value;
+
+    if (/[\d]+/.test(debitBalance) && bank.length > 0) {
+        bank[bank.length - 1].debitAccounts.push(new Account(addDebitCurrency.value, debitBalance));
+        resultDebAccount.innerHTML = `Дебетовый счет в ${addDebitCurrency.value} на сумму ${debitBalance}`;
+    } else {
+        resultDebAccount.innerHTML = 'Данные для ввода не корректны';
     }
+    event.target.reset();
+});
 
-    numbersDebtors(arrayBankCustomers, activityType) {
-        let count = 0;
-        arrayBankCustomers.forEach(client => {
-            if (client.isActive === activityType) {
-                client.creditAccounts.forEach(account => {
-                    if ((account.creditLimit - account.balance) < 0) {
-                        count++;
-                    }
-                });
+addFormCreditAccount.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let creditBalance = +addCreditBalance.value;
+    let creditLimit = +addCreditLimit.value;
+    if (/[\d]+/.test(creditBalance) && /[\d]+/.test(creditLimit) && bank.length > 0) {
+        bank[bank.length - 1].creditAccounts.push(new Account(addCreditCurrency.value, creditBalance, creditLimit));
+        resultCreditAccount.innerHTML = ` Кредитовый счет в ${addCreditCurrency.value} на сумму ${creditBalance} с лимитом ${creditLimit}`;
+    } else {
+        resultCreditAccount.innerHTML = 'Данные для ввода не корректны';
+    }
+    event.target.reset();
+});
+
+findForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    resultFinding.innerHTML = 'Не является клиентом банка';
+    clearFindingForm();
+    for (let i = 0; i < bank.length; i++) {
+        if (bank[i].id === idByFind.value) {
+            resultFinding.innerHTML = `ID - ${bank[i].id} , Name - ${bank[i].name} , isActive - ${bank[i].isActive}`;
+            for (let j = 0; j < bank[i].debitAccounts.length; j++) {
+                resultFindingDebAccount.innerHTML +=
+                    `Дебетовый счет в ${bank[i].debitAccounts[j].currency} на сумму ${bank[i].debitAccounts[j].balance} <br>`;
             }
-        });
-        return count;
+            for (let j = 0; j < bank[i].creditAccounts.length; j++) {
+                resultFindingCreditAccount.innerHTML +=
+                    `Кредитовый счет в ${bank[i].creditAccounts[j].currency} на сумму ${bank[i].creditAccounts[j].balance} с лимитом ${bank[i].creditAccounts[j].creditLimit}<br>`;
+            }
+            document.querySelector('#del').addEventListener('click', () => {
+                resultFinding.innerHTML = '';
+                clearFindingForm();
+                bank.splice(i, 1);
+            });
+        }
     }
+    event.target.reset();
+});
 
-    async sumDebitInactiveClients(arrayBankCustomers, activityType) {
-        let sum = 0;
-        let debtBalance = 0;
-        await this.requestExchangeRate().then(exchangeRates => {
-            arrayBankCustomers.forEach(client => {
-                if (client.isActive === activityType) {
-                    client.creditAccounts.forEach(account => {
-                        if ((account.creditLimit - account.balance) < 0) {
-                            debtBalance = account.balance - account.creditLimit;
-                            sum += this.currencyConversion(account, debtBalance, exchangeRates);
-                        }
-                    });
+function clearFindingForm() {
+    resultFindingDebAccount.innerHTML = '';
+    resultFindingCreditAccount.innerHTML = '';
+}
+
+async function bankMoney(arrayBankCustomers) {
+    let sum = 0;
+    await requestExchangeRate().then(exchangeRates => {
+        arrayBankCustomers.forEach(client => {
+            [...client.creditAccounts, ...client.debitAccounts].forEach(account => sum += currencyConversion(account, account.balance, exchangeRates));
+        });
+    });
+    totalResult.innerHTML = sum.toFixed(2);
+}
+
+async function bankDebit(arrayBankCustomers) {
+    let sum = 0;
+    let debtBalance = 0;
+    await requestExchangeRate().then(exchangeRates => {
+        arrayBankCustomers.forEach(client => {
+            client.creditAccounts.forEach(account => {
+                if (account.creditLimit > account.balance) {
+                    debtBalance = account.creditLimit - account.balance;
+                    sum += currencyConversion(account, debtBalance, exchangeRates);
                 }
             });
         });
-        return sum;
-    }
+    });
+    totalResult.innerHTML = sum.toFixed(2);
+}
 
-    async requestExchangeRate() {
-        let promise = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
-        return await promise.json();
-    }
+function numbersDebtors(arrayBankCustomers, activityType) {
+    let count = 0;
+    arrayBankCustomers.forEach(client => {
+        if (client.isActive === activityType) {
+            client.creditAccounts.forEach(account => {
+                if ((account.balance - account.creditLimit) < 0) {
+                    count++;
+                }
+            });
+        }
+    });
+    totalResult.innerHTML = count;
+}
 
-    currencyConversion(account, balance, exchangeRates, out) {
-        let result = 0;
-        let coef = 1;
-        out = out || 'USD';
-        exchangeRates.forEach(eachCurrency => {
-            if (account.currency === eachCurrency.ccy) {
-                exchangeRates.forEach(eachCurrency => {
-                    if (out === eachCurrency.ccy) {
-                        coef = eachCurrency.buy;
+async function sumDebitInactiveClients(arrayBankCustomers, activityType) {
+    let sum = 0;
+    let debtBalance = 0;
+    await requestExchangeRate().then(exchangeRates => {
+        arrayBankCustomers.forEach(client => {
+            if (client.isActive === activityType) {
+                client.creditAccounts.forEach(account => {
+                    if ((account.balance - account.creditLimit) < 0) {
+                        debtBalance = account.creditLimit - account.balance;
+                        sum += currencyConversion(account, debtBalance, exchangeRates);
                     }
                 });
-                result = balance * eachCurrency.buy / coef;
-            } else if (eachCurrency.base_ccy === account.currency && eachCurrency.ccy === out) {
-                result = balance / eachCurrency.buy;
             }
         });
-        return result;
-    }
+    });
+    totalResult.innerHTML = sum.toFixed(2);
 }
+
+async function requestExchangeRate() {
+    let promise = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+    return await promise.json();
+}
+
+function currencyConversion(account, balance, exchangeRates, out) {
+    let result = 0;
+    let coef = 1;
+    out = out || 'USD';
+    exchangeRates.forEach(eachCurrency => {
+        if (account.currency === eachCurrency.ccy) {
+            exchangeRates.forEach(eachCurrency => {
+                if (out === eachCurrency.ccy) {
+                    coef = eachCurrency.buy;
+                }
+            });
+            result = balance * eachCurrency.buy / coef;
+        } else if (eachCurrency.base_ccy === account.currency && eachCurrency.ccy === out) {
+            result = balance / eachCurrency.buy;
+        }
+    });
+    return result;
+}
+
+buttonSumBankMoney.addEventListener('click', function () {
+    bankMoney(bank);
+});
+
+buttonSumDebitBank.addEventListener('click', function () {
+    bankDebit(bank);
+});
+
+buttonNumbersDebitors.addEventListener('click', function () {
+    numbersDebtors(bank, false);
+});
+
+buttonSumDebitIsactiveClients.addEventListener('click', function () {
+    sumDebitInactiveClients(bank, false);
+});
